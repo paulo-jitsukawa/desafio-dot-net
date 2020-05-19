04413e1770950a53b91474b1f0d242ccdc1c53ec
Paulo Masaru Meneghel Jitsukawa <paulo.jitsukawa@edu.pucrs.br>
2020-05-19 14:19:06 -0300

    Cria a camada Exceptions

---
+++ DesafioDotNet/Sales/Exceptions/FileInvalidFormatException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Sales.Exceptions
+{
+    public class FileInvalidFormatException : Exception
+    {
+        public int Line { get; private set; }
+        public string Name { get; private set; }
+
+        public FileInvalidFormatException(string message, int line, string name, Exception inner = null) : base(message, inner)
+        {
+            Line = line;
+            Name = name;
+        }
+    }
+}
