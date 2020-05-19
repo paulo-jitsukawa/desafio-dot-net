6b25901c58d49eddbd7c2e19b3f759551e1a101f
Paulo Masaru Meneghel Jitsukawa <paulo.jitsukawa@edu.pucrs.br>
2020-05-19 13:25:28 -0300

    Cria projeto Console do DotNetCore 3.1 no Visual Studio 2019

---
+++ DesafioDotNet/Sales.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sales.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
