461ae6f1973ca77ec1ccda5bc491aacad5b0d461
Paulo Masaru Meneghel Jitsukawa <paulo.jitsukawa@edu.pucrs.br>
2020-05-20 09:24:38 -0300

    Cria a camada Extensions

---
+++ DesafioDotNet/Sales/Extensions/PathExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace Sales.Extensions
+{
+    public static class PathExtensions
+    {
+        public static string ToNormalizedPath(this string path)
+        {
+            return Path.DirectorySeparatorChar == '/'
+                ? path.Replace('\\', '/').TrimEnd('/')
+                : path.Replace('/', '\\').TrimEnd('\\');
+        }
+    }
+}
